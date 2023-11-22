344d05675107a851b8fd83c8a0ceba651897dfe9
Abdulrahman Aroworamimo <109465283+Abdul-AA@users.noreply.github.com>
2023-11-22 13:50:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ def create_dataframe_updated():
         # Updated data as per the provided model results
     data_2025 = {
             'Year': [2025] * 8,
-            'Source': ['Wind', 'Solar', 'Oil', 'Nuclear', 'Hydro', 'Natural Gas', 'Coal & Coke', 'Biomass & Geothermal'],
+            'Source': ['Wind', 'Solar', 'Oil', 'Nuclear', 'Hydro', 'Natural Gas', 'Coal', 'Geothermal'],
             'Generation (GWh)': [64389.48, 11402.425080834817, 1379.91, 78631.37, 402575.9, 45146.6549191651, 0.0, 8281.2],
             'Emissions (MTCO2e)': [0.9658422, 0.5074079160971493, 0.6761559, 0.94357644, 9.6618216, 22.121860910390897, 0.0, 0.3146856],
             'Generation Cost (CAD)': [5204601668.4, 2343198354.111555, 85071451.5, 7002123498.5, 12133637626.0, 4391415123.987189, 0.0, 680714640.0],
@@ -19,7 +19,7 @@ def create_dataframe_updated():
 
     data_2030 = {
         'Year': [2030] * 8,
-        'Source': ['Wind', 'Solar', 'Oil', 'Nuclear', 'Hydro', 'Natural Gas', 'Coal & Coke', 'Biomass & Geothermal'],
+        'Source': ['Wind', 'Solar', 'Oil', 'Nuclear', 'Hydro', 'Natural Gas', 'Coal', 'Geothermal'],
         'Generation (GWh)': [151989.48, 0.0, 1379.91, 130180.19, 402575.9, 0.0, 0.0, 8281.2],
         'Emissions (MTCO2e)': [2.2798422, 0.0, 0.6761559, 1.56216228, 9.6618216, 0.0, 0.0, 0.3146856],
         'Generation Cost (CAD)': [12285309668.4, 0.0, 85071451.5, 11592545919.5, 12133637626.0, 0.0, 0.0, 680714640.0],
@@ -28,7 +28,7 @@ def create_dataframe_updated():
     
     data_2035 = {
         'Year': [2035] * 8,
-        'Source': ['Wind', 'Solar', 'Oil', 'Nuclear', 'Hydro', 'Natural Gas', 'Coal & Coke', 'Biomass & Geothermal'],
+        'Source': ['Wind', 'Solar', 'Oil', 'Nuclear', 'Hydro', 'Natural Gas', 'Coal', 'Geothermal'],
         'Generation (GWh)': [239589.48000000004, 0.0, 0.0, 271351.37, 276900.2599999999, 0.0, 0.0, 0.0],
         'Emissions (MTCO2e)': [3.5938422000000005, 0.0, 0.0, 3.25621644, 6.6456062399999976, 0.0, 0.0, 0.0],
         'Generation Cost (CAD)': [19366017668.4, 0.0, 0.0, 24163839498.5, 8345773836.399997, 0.0, 0.0, 0.0],
